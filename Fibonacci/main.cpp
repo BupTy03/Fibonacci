c83b5e745a0a065006f0de20adbb09f12e921321
Anton <anton.lagosha.99@mail.ru>
2020-04-19 11:54:52 +0500

    make_fibonacci_list function added

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -5,17 +5,43 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <vector>
+#include <numeric>
 
 
 using cpp_int = boost::multiprecision::cpp_int;
 
 void print_fibonacci(int number);
+void print_fibonacci(int from, int count);
+
+template<typename T>
+auto make_fibonacci_list(std::size_t num)
+{
+	std::vector<T> v(num, 1);
+	std::adjacent_difference(std::begin(v), std::prev(std::end(v)), std::next(std::begin(v)), std::plus<>());
+	return v;
+}
+
+template<typename ForwardIt>
+void print_list(ForwardIt first, ForwardIt last)
+{
+	std::copy(first, last,
+		std::ostream_iterator<typename std::iterator_traits<ForwardIt>::value_type>(std::cout, ", "));
+}
+
+template<typename Container>
+void print_list(const Container& cont)
+{
+	print_list(std::begin(cont), std::end(cont));
+}
+
 
 int main()
 {
 	std::system("chcp 1251");
 
-	std::copy_n(fibonacci_coroutine<cpp_int>(50).begin(), 10, std::ostream_iterator<cpp_int>(std::cout, ", "));
+	const auto fibonacci_numbers = make_fibonacci_list<int>(20);
+	print_list(fibonacci_numbers);
 	return 0;
 }
 
@@ -23,4 +49,10 @@ int main()
 void print_fibonacci(int number)
 {
 	std::cout << "Число Фибоначчи от " << number << ": " << fibonacci<cpp_int>(number) << std::endl;
+}
+
+void print_fibonacci(int from, int count)
+{
+	std::copy_n(fibonacci_coroutine<cpp_int>(from).begin(), count, 
+		std::ostream_iterator<cpp_int>(std::cout, ", "));
 }
